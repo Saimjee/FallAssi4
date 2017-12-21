3a068a523e1e4b628c0915a3499213ac62469df8
sunlight1 <awesomesaim@gmail.com>
2017-12-22 01:48:33 +0500

    Initial commit

---
+++ app/src/main/java/com/example/sunlight1/fallassi4/EventMessage.java
@@ -0,0 +1,19 @@
+package com.example.sunlight1.fallassi4;
+
+/**
+ * Created by sunlight1 on 12/22/2017.
+ */
+
+public class EventMessage {
+
+    private String serviceTime;
+
+    public String getServiceTime() {
+        return serviceTime;
+    }
+
+    public void setServiceTime(String serviceTime) {
+        this.serviceTime = serviceTime;
+    }
+}
+
